90a8aae0ade55c4118347d7c64f8abac04ba1579
RockyPraxe <filipio.svanda@gmail.com>
2023-08-21 13:59:25 +0000

    changes

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
     //  sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n:{done: "Select"}
+    });
+    
+    // select initialization
+    let select = document.querySelectorAll('select');
+    M.FormSelect.init(select);
+
+    // collapsible initialization
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
 });
